71274d06eabb7940d19c4ba912107b580e623d8d
Diksha <158588723+diksha-jeena@users.noreply.github.com>
2024-07-01 23:53:32 +0530

    Add files via upload

---
+++ Stack/stackToQueue.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+stack = []
+
+class Queue:
+    def __init__(self):
+        self.queue = deque()
+
+def push(Stack):
+    stack.append(item)
+
+def display(Stack):
+    while(Stack.peek() == None):
+        return Stack
+
+def enqueue(self, item):
+    self.queue.append(item)
+
+def display(Queue):
+    while(Queue.peek() == None):
+        return Queue    
+
+Stack = stack()
+queue = Queue()
+
+stack.push(8)
+stack.push(5)
+stack.push(7)
+stack.push(2)
+
+while not stack.is_empty():
+    item = stack.pop()
+    queue.enqueue(item)
+
+queue.display(Stack)
