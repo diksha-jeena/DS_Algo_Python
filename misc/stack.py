e11b1ff446cdfaeaad03d3a060709db8a2dfbd05
Diksha <158588723+diksha-jeena@users.noreply.github.com>
2024-08-07 23:14:17 +0530

    stack implementation

---
+++ misc/stack.py
@@ -0,0 +1,36 @@
+stack = []
+
+def push(stack , item):
+    if type(item)==int:
+        stack.append(item)
+    else:
+        for i in item:
+            stack.append(i)
+
+def pop(stack):
+    if not is_empty(stack):
+        return stack.pop()
+    return None 
+
+def peek(stack):
+    if not is_empty(stack):  
+        return stack[-1]
+    
+def is_empty(stack):
+    return len(stack) == 0 
+
+def size(stack):
+    return len(stack)
+
+def display(stack):
+    while(peek(stack) == None):
+        return stack
+
+lst = [2,3,4,5]
+push(stack ,lst)
+push(stack , 5)
+push(stack , 2)
+pop(stack)
+print(is_empty(stack))
+print(size(stack))
+print(stack)
